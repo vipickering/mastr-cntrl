3024231e069e8d94234356b86bb535195a604f03
vipickering <hello@vincentp.me>
2018-07-28 09:53:36 +0100

    :construction: added indieauth token security to syndication GET

--- routes/routes.js
+++ routes/routes.js
@@ -36,11 +36,32 @@ const syndicateOptions = {
 };
 
 router.get('/micropub', (req, res) => {
-    if (req.query.q == 'syndicate-to') {
-        res.json(syndicateOptions);
-    } else {
-         res.json(serviceProfile);
-    }
+    const token = req.headers.authorization;
+    const indieauth = 'https://tokens.indieauth.com/token';
+    const authHeaders = {
+        'Accept' : 'application/json',
+        'Authorization' : token
+    };
+    logger.info('Token Received: ' + token);
+
+    fetch(indieauth, {
+        method : 'GET',
+        headers : authHeaders
+    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            serviceIdentifier = json.client_id;
+            logger.info('Service Is: ' + serviceIdentifier);
+
+            if ((req.query.q == 'syndicate-to') && (serviceIdentifier == 'https://quill.p3k.io/')) {
+                res.json(syndicateOptions);
+            } else {
+                 res.json({});
+            }
+
+      });
 });
 
 // Catch any illegal routes
