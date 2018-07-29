2bae56708ab1ee85b7d8165708a00cea0f659c26
vipickering <hello@vincentp.me>
2018-07-29 08:51:22 +0100

    :construction: added syndicated-to param to notes

--- functions/format-note.js
+++ functions/format-note.js
@@ -7,27 +7,6 @@ exports.note = function note(micropubContent) {
     const category = 'Notes';
     const pubDate  = moment(new Date()).format('YYYY-MM-DDTHH:mm:ss+01:00');
 
-    // const syndication = micropubContent.properties.syndication[0]; // Might add this later
-
-    /*
-    h=entry - This indicates that this is a request to create a new h-entry post.
-    content - The text of your post. Your endpoint is expected to treat this as plaintext, and handle all escaping as necessary.
-    category[] - This property will be repeated for each tag you've entered in the "tags" field.
-    in-reply-to - If you tap the Reply button and enter a URL, the URL will be sent in this property.
-    location - If you check the "location" box, then this property will be a Geo URI with the location the browser detected. You will see a preview of the value in the note interface along with a map.
-    photo or photo[] - If your server supports a Media Endpoint, this will be set to the URL that your endpoint returned when it uploaded the photo. Otherwise, this will be one of the parts in the multipart request with the image file itself.
-    mp-slug - If you enter a slug, this will be sent in the request. You can customize the name of this property in settings.
-    mp-syndicate-to[] - Each syndication destination selected will be sent in this property. The values will be the uid that your endpoint returns. See Syndication for more details. (If you are using an older Micropub endpoint that expects syndicate-to, you can customize this property in the settings.)
-    */
-
-    /*
-    "content":"quill test 3",
-    "location":"geo:53.80178,-1.54731;u=65",
-    "category":["test","indieweb"],
-    "mp-slug":"test"
-
-    */
-
     let content = '';
     let inReplyTo = '';
     let location = '';
@@ -35,6 +14,7 @@ exports.note = function note(micropubContent) {
     let tags = '';
     let tagArray = '';
     let title = '';
+    let syndication = '';
 
     try {
         content = micropubContent.content;
@@ -64,13 +44,14 @@ exports.note = function note(micropubContent) {
         logger.info('No location skipping');
     }
 
-    // try {
-    //     photo = micropubContent.properties.content[0].html;
-    // } catch (e) {
-    //     logger.info('No content skipping..');
-    // }
+    try {
+        syndication = micropubContent['mp-syndicate-to'][0];
+    } catch (e) {
+        logger.info('No Syndication skipping');
+    }
 
-    // photo & location  need conditional
+//Photo and location not being supported. I have no need for them.
+
     const entry = `---
 layout: "${layout}"
 title: "${title}"
@@ -78,6 +59,7 @@ date: "${pubDate}"
 meta: "${title}"
 category: "${category}"
 tags:  "${tags}"
+syndication:  "${syndication}"
 twitterCard: false
 ---
 ${content}
