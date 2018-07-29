91ed8dd790f20c474a3b72caeaff40b8ef0e5dfd
vipickering <hello@vincentp.me>
2018-07-29 22:55:27 +0100

    :fire: removing commented out code

--- bin/server.js
+++ bin/server.js
@@ -11,10 +11,6 @@ const config = require(appRootDirectory + '/app/config.js');
 const logger = require(appRootDirectory + '/app/functions/bunyan');
 const routes = require(appRootDirectory + '/app/routes.js');
 
-// const config = require(__dirname + '/config');
-// const logger = require(__dirname + '/app/functions/bunyan');
-// const routes  = require(__dirname + '/app/routes.js');
-
 const github = config.github;
 const api = config.api;
 const app = express();
@@ -23,7 +19,6 @@ const isDev = app.get('env') === 'development';
 
 const upload = multer();
 
-// app.set('views', path.join(__dirname + '/views'));
 app.use(helmet());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
