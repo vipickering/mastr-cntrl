3dc91a62aee6cbd0550e4d8244461ad669908c51
vipickering <hello@vincentp.me>
2018-11-06 21:54:45 +0000

    :bug: adding commented file to prevent fall over

---
+++ app/routes/post/media.js
@@ -0,0 +1,18 @@
+// moment
+// const multer = require('multer');
+// const upload = multer({
+//     dest: 'images/blog/' + date,
+//     fileFilter: jpg, jpeg, gif, png // do we need disk storage here?
+// }); //should recieve multipart/form-data
+
+// exports.mediaPost = function mediaPost(req, res) {
+// // How to pass auth along authentication?
+// // 1 or more photos? need both use cases
+
+// // Indieauth check, based on prior micropub GET request. If No, end.
+// // Need to get the image, and upload in to Github API.
+// // File should go in  images/blog/date/photo.jpg (photo1 photo2 etc).
+
+//     res.status(200);
+//     res.send('Working');
+// };
