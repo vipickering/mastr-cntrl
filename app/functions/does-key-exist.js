9cde03f2afef23ee982059a098bc17f01d23006b
vipickering <hello@vincentp.me>
2018-12-01 10:45:06 +0000

    :fire: removed old code

--- app/functions/does-key-exist.js
+++
@@ -1,9 +0,0 @@
-exports.doesExist = function doesExist(variable, value) {
-    try {
-        variable = value;
-    } catch (e) {
-        logger.info('No ' + variable + ' skipping..');
-    }
-};
-
-// This needs more thought to be implemented.
