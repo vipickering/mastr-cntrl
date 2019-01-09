0c07b1c37f71b5f4f128cd59631266dc1cf5734d
vipickering <hello@vincentp.me>
2019-01-09 10:06:14 +0000

    :loud_sound: updating logging

--- app/routes/post/micropub.js
+++ app/routes/post/micropub.js
@@ -30,6 +30,8 @@ exports.micropubPost = function micropubPost(req, res) {
     //Log packages sent, for debug
     logger.info(`Request Body: ${JSON.stringify(req.body)}`);
     logger.info(`Request Header: ${JSON.stringify(req.headers)}`);
+    logger.info(`request: ${req}`);
+    logger.info(`response: ${res}`);
     logger.info(`Authorization Token: ${token}`);
     logger.info(`Incoming Token: ${req.body.access_token}`);
 
