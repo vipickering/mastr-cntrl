ace60649b25b87679dc90ac1b33cccb85d0fd312
vipickering <hello@vincentp.me>
2019-01-09 10:12:36 +0000

    :loud_sound: tweaking log to parse JSON

--- app/routes/post/micropub.js
+++ app/routes/post/micropub.js
@@ -30,8 +30,8 @@ exports.micropubPost = function micropubPost(req, res) {
     //Log packages sent, for debug
     logger.info(`Request Body: ${JSON.stringify(req.body)}`);
     logger.info(`Request Header: ${JSON.stringify(req.headers)}`);
-    logger.info(`request: ${JSON.stringify(req)}`);
-    logger.info(`response: ${JSON.stringify(res)}`);
+    logger.info(`request: ${JSON.parse(req)}`);
+    logger.info(`response: ${JSON.parse(res)}`);
     logger.info(`Authorization Token: ${token}`);
     logger.info(`Incoming Token: ${req.body.access_token}`);
 
