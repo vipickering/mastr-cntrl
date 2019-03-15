b19c7e0d5cab5652eab6eee45eeee18b1e92335c
vipickering <hello@vincentp.me>
2019-03-15 14:49:18 +0000

    :construction: upgrading media endpoint to promises

--- app/routes/post/media.js
+++ app/routes/post/media.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const multer = require('multer');
-const request = require('request');
-const base64 = require('base64it');
+// const request = require('request');
+// const base64 = require('base64it');
 const moment = require('moment');
 const shortid = require('shortid');
 const storage = multer.memoryStorage();
@@ -9,15 +9,17 @@ const upload = multer({storage : storage});
 const config = require(appRootDirectory + '/app/config.js');
 const github = config.github;
 const logger = require(appRootDirectory + '/app/functions/bunyan');
+const githubApi = require(appRootDirectory + '/app/functions/githubApi');
 
 exports.mediaPost = function mediaPost(req, res) {
     const publishedDate = moment(new Date()).format('YYYY-MM-DD');
-    const filename = shortid.generate();
-    const photoName = `${filename}.jpg`; //Need to identify other mimetypes
-    const payload = base64.encode(req.files[0].buffer);
-    const messageContent = ':robot: Media submitted by Mastrl Cntrl';
-    const responseLocation = `https://vincentp.me/images/blog/${publishedDate}/${photoName}`;
-    const postDestination = `${github.postUrl}/contents/images/blog/${publishedDate}/${photoName}`;
+    const filenameID = shortid.generate();
+    const fileName = `${filenameID}.jpg`; //Need to identify other mimetypes
+    // const payload = base64.encode(req.files[0].buffer);
+    // const messageContent = ':robot: Media submitted by Mastrl Cntrl';
+    const responseLocation = `https://vincentp.me/images/blog/${publishedDate}/${fileName}`;
+    const fileLocation = `images/blog/${publishedDate}`;
+    const postDestination = `${github.postUrl}/contents/images/blog/${publishedDate}/${fileName}`;
     let token;
     let formattedToken;
     const indieauth = 'https://tokens.indieauth.com/token';
@@ -25,26 +27,26 @@ exports.mediaPost = function mediaPost(req, res) {
         'Accept' : 'application/json',
         'Authorization' : token
     };
-    const payloadOptions = {
-        method : 'PUT',
-        url : postDestination,
-        headers : {
-            Authorization : `token ${github.key}`,
-            'Content-Type' : 'application/vnd.github.v3+json; charset=UTF-8',
-            'User-Agent' : github.name
-        },
-        body : {
-            path : photoName,
-            branch : github.branch,
-            message : messageContent,
-            committer : {
-                'name' : github.user,
-                'email' : github.email
-            },
-            content : payload
-        },
-        json : true
-    };
+    // const payloadOptions = {
+    //     method : 'PUT',
+    //     url : postDestination,
+    //     headers : {
+    //         Authorization : `token ${github.key}`,
+    //         'Content-Type' : 'application/vnd.github.v3+json; charset=UTF-8',
+    //         'User-Agent' : github.name
+    //     },
+    //     body : {
+    //         path : fileName,
+    //         branch : github.branch,
+    //         message : messageContent,
+    //         committer : {
+    //             'name' : github.user,
+    //             'email' : github.email
+    //         },
+    //         content : payload
+    //     },
+    //     json : true
+    // };
 
     try {
         token = req.headers.authorization;
@@ -64,24 +66,24 @@ exports.mediaPost = function mediaPost(req, res) {
             return responseLocation;
     }
 
-    function sendtoGithub(error, response, body) {
-        if (error) {
-            res.status(400);
-            res.send('Error Sending Image');
-            logger.error(`Git creation failed: ${error}`);
-            res.end('Error Sending Image');
-            throw new Error(`Failed to send: ${error}`);
-        } else {
-            res.writeHead(201, {
-                'location' : responseLocation
-            });
-            logger.info('Git creation successful!  Server responded with:', body);
-            res.end('Thanks');
-        }
-    }
+    // function sendtoGithub(error, response, body) {
+    //     if (error) {
+    //         res.status(400);
+    //         res.send('Error Sending Image');
+    //         logger.error(`Git creation failed: ${error}`);
+    //         res.end('Error Sending Image');
+    //         throw new Error(`Failed to send: ${error}`);
+    //     } else {
+    //         res.writeHead(201, {
+    //             'location' : responseLocation
+    //         });
+    //         logger.info('Git creation successful!  Server responded with:', body);
+    //         res.end('Thanks');
+    //     }
+    // }
 
-    logger.info(`response location: ${responseLocation}`);
-    logger.info(`postDestination destination: ${postDestination}`);
+    // logger.info(`response location: ${responseLocation}`);
+    // logger.info(`postDestination destination: ${postDestination}`);
 
     // Verify Token. If OK proceed.
     fetch(indieauth, {
@@ -89,6 +91,7 @@ exports.mediaPost = function mediaPost(req, res) {
         headers : authHeaders
     })
         .then(authResponse)
-        .then(request(payloadOptions, sendtoGithub))
+        // .then(request(payloadOptions, sendtoGithub))
+        .then(githubApi.publish(req, res, fileLocation, fileName, responseLocation, payload))
         .catch((err) => logger.error(err));
 };
