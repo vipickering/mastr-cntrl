f610f87f68ee1830022d56a12e9b7b67415043a7
vipickering <hello@vincentp.me>
2020-03-28 11:55:20 +1300

    :alembic: testing out refactoring

--- app/micropub/process-content/content.js
+++ app/micropub/process-content/content.js
@@ -4,13 +4,14 @@ const logger = require(appRootDirectory + '/app/logging/bunyan');
 Check for Micropub main content and catch if there has been none sent.
 */
 exports.formatContent = function formatContent(micropubContent) {
-    let content = '';
-
+    let content = micropubContent.content;
     logger.info('Checking for text content');
-    try {
-        content = micropubContent.content;
-    } catch (e) {
+
+    if (content !== '') {
+        logger.info('Content found');
+    } else {
         logger.info('No content micropubContent.content');
+        content = '';
     }
 
     return content;
