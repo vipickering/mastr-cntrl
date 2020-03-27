abc67acbfbe0da805a9fdf8f65873b0fe913579e
vipickering <hello@vincentp.me>
2020-03-28 11:58:25 +1300

    :alembic: testing out refactoring

--- app/micropub/process-content/tags.js
+++ app/micropub/process-content/tags.js
@@ -7,11 +7,13 @@ exports.formatTags = function formatTags(micropubContent) {
     let tags = '';
     let tagArray = '';
 
+    logger.info('Checking for tags');
     try {
         tagArray = micropubContent.category;
         for (let i = 0; i < tagArray.length; i++) {
             tags += '\n- ';
             tags += tagArray[i];
+            logger.info('Found tag' + tagArray[i]);
         }
     } catch (e) {
         logger.info('No tags provided assigning miscellaneous');
