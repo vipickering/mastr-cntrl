5b4a540b96e843dc5b2eab924a0ddabe03e434a5
vipickering <hello@vincentp.me>
2020-03-28 12:01:23 +1300

    :alembic: testing out refactoring

--- app/micropub/process-content/alt-text.js
+++ app/micropub/process-content/alt-text.js
@@ -5,16 +5,16 @@ Check for existance of photos and if they exist, create the URLS.
 The Media endpoint handles the actual image, we don't need to bother with that here
 */
 exports.formatAltText = function formatAltText(micropubContent) {
-    const photoArray = micropubContent.properties.photo;
     let alt = '';
 
     logger.info('Checking for alt text');
     try {
+        const photoArray = micropubContent.properties.photo;
+
         for (let j = 0; j < photoArray.length; j++) {
             alt += `photo${j + 1}_alt: "${photoArray[j].alt}"\n`;
         }
     } catch (e) {
-        alt = '';
         logger.info('No alt text provided');
     }
 
