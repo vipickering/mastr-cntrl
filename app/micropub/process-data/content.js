6b19430857d8230079c13e09971cfc31d6cf03cd
vipickering <hello@vincentp.me>
2020-03-28 17:26:34 +1300

    :construction: added conditons and error handling and logging for note content

--- app/micropub/process-data/content.js
+++ app/micropub/process-data/content.js
@@ -4,8 +4,23 @@ const logger = require(appRootDirectory + '/app/logging/bunyan');
 Check for Micropub main content and catch if there has been none sent.
 */
 exports.formatContent = function formatContent(micropubContent) {
-    let content = micropubContent.content;
-    logger.info('Checking for text content');
+    let content = '';
+
+    // Content is sent from Quil in different places if its a photo note or a standard note.
+    logger.info('Checking for text content,');
+    try {
+        content = micropubContent.content;
+        logger.info('Content is basic note content');
+    } catch (e) {
+        logger.info('No basic note content provided');
+    }
+
+    try {
+        content = micropubContent.properties.content;
+        logger.info('Content is photo note content');
+    } catch (e) {
+        logger.info('No photo note content');
+    }
 
     if (content !== '') {
         logger.info('Content found');
