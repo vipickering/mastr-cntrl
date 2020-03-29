1955c5ff521872792d90c8a3a214d35f2dd28051
vipickering <hello@vincentp.me>
2020-03-29 17:24:06 +1300

    :construction: syndication added

--- app/micropub/process-data/syndication-targets.js
+++ app/micropub/process-data/syndication-targets.js
@@ -1,8 +1,7 @@
 const logger = require(appRootDirectory + '/app/logging/bunyan');
 
 /*
-Check for existance of photos and if they exist, create the URLS.
-The Media endpoint handles the actual image, we don't need to bother with that here
+Check for existance of syndication targets. If they exist output them.
 */
 exports.formatTargets = function formatTargets(micropubContent) {
     let targets = '';
