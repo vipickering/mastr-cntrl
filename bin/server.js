7cfd222b5dc7de179c59fc8af441769377007625
vipickering <hello@vincentp.me>
2020-04-07 16:09:03 +1200

    :construction: fixing server file

--- bin/server.js
+++ bin/server.js
@@ -4,20 +4,16 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 
 appRootDirectory = path.join(__dirname, '/..');
 const config = require(appRootDirectory + '/app/config.js');
 const logger = require(appRootDirectory + '/app/logging/bunyan');
 const routes = require(appRootDirectory + '/app/routes.js');
 
-const github = config.github;
 const api = config.api;
 const app = express();
 const port = api.port;
 // const isDev = app.get('env') === 'development';
-
-const upload = multer();
 
 app.use(helmet());
 app.use(bodyParser.json()); // for parsing application/json
@@ -27,6 +23,6 @@ app.use(express.json());
 app.use('/', routes);
 
 /*eslint-disable-next-line no-process-env */
-const server = app.listen(process.env.PORT || 3000, function () {
+const server = app.listen(process.env.PORT || port, function () {
     logger.info('Mastr Cntrl Online Port:%s...', server.address().port);
 });
