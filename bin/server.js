bc62fb67e3816187039292cccb3bee206dcb83af
vipickering <hello@vincentp.me>
2020-04-07 16:06:01 +1200

    :bug: reverting server file

--- bin/server.js
+++ bin/server.js
@@ -4,23 +4,29 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+
 appRootDirectory = path.join(__dirname, '/..');
 const config = require(appRootDirectory + '/app/config.js');
 const logger = require(appRootDirectory + '/app/logging/bunyan');
 const routes = require(appRootDirectory + '/app/routes.js');
+
+const github = config.github;
 const api = config.api;
-const port = api.port;
 const app = express();
+const port = api.port;
 // const isDev = app.get('env') === 'development';
 
+const upload = multer();
+
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(favicon('public/images/favicon.ico'));
 app.use(express.json());
 app.use('/', routes);
 
 /*eslint-disable-next-line no-process-env */
-const server = app.listen(port, function serveTheThings() {
+const server = app.listen(process.env.PORT || 3000, function () {
     logger.info('Mastr Cntrl Online Port:%s...', server.address().port);
 });
