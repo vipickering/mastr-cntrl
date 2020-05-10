6c73ca1d054960b90ce05a4dfc399909d3c48afa
vipickering <hello@vincentp.me>
2020-05-10 19:59:02 +1200

    :construction: replace request-promise code with axios. Ready for testing

--- app/github/post-to-api.js
+++ app/github/post-to-api.js
@@ -2,7 +2,7 @@
 POST content to the Github API
 */
 
-const rp = require('request-promise');
+const axios = require('axios');
 const base64 = require('base64it');
 const logger = require(appRootDirectory + '/app/logging/bunyan');
 const config = require(appRootDirectory + '/app/config.js');
@@ -12,45 +12,79 @@ exports.publish = function publish(req, res, fileLocation, fileName, responseLoc
     const payloadEncoded = base64.encode(payload);
     const fileDestination = `${github.postUrl}/contents/${fileLocation}/${fileName}`;
     const messageContent = `:robot: ${commitMessage}`;
-    const options = {
-        method : 'PUT',
-        url : fileDestination,
-        headers : {
-            Authorization : `token ${github.key}`,
-            'Content-Type' : 'application/vnd.github.v3+json; charset=UTF-8',
-            'User-Agent' : github.name
-        },
-        body : {
-            path : fileName,
-            branch : github.branch,
-            message : messageContent,
-            committer : {
-                'name' : github.user,
-                'email' : github.email
+    // const options = {
+    //     method : 'PUT',
+    //     url : fileDestination,
+    //     headers : {
+    //         Authorization : `token ${github.key}`,
+    //         'Content-Type' : 'application/vnd.github.v3+json; charset=UTF-8',
+    //         'User-Agent' : github.name
+    //     },
+    //     body : {
+    //         path : fileName,
+    //         branch : github.branch,
+    //         message : messageContent,
+    //         committer : {
+    //             'name' : github.user,
+    //             'email' : github.email
+    //         },
+    //         content : payloadEncoded
+    //     },
+    //     json : true
+    // };
+
+    // function successful() {
+    //     logger.info('Git creation successful!');
+    //     res.writeHead(201, {'location' : responseLocation});
+    //     res.end('Thanks');
+    // }
+
+    // function githubError(err) {
+    //     res.status(400);
+    //     res.send('Update failed');
+    //     logger.info('POST to Github API Failed');
+    //     logger.error(err);
+    //     res.end('Error Sending Payload');
+    // }
+
+    // logger.info(`Response: ${responseLocation}`);
+    // logger.info(`Destination: ${fileDestination}`);
+
+    // rp(options)
+    //     .then(successful)
+    //     .catch(githubError);
+
+
+    (async () => {
+        try {
+        const options = {
+            method : 'PUT',
+            url : fileDestination,
+            headers : {
+                Authorization : `token ${github.key}`,
+                'Content-Type' : 'application/vnd.github.v3+json; charset=UTF-8',
+                'User-Agent' : github.name
             },
-            content : payloadEncoded
-        },
-        json : true
-    };
+            data : {
+                path : fileName,
+                message : messageContent,
+                branch : github.branch,
+                content : payloadEncoded,
+                committer : {
+                    name : github.user,
+                    email : github.email
+                }
+            }
+        };
 
-    function successful() {
-        logger.info('Git creation successful!');
-        res.writeHead(201, {'location' : responseLocation});
-        res.end('Thanks');
-    }
-
-    function githubError(err) {
-        res.status(400);
-        res.send('Update failed');
-        logger.info('POST to Github API Failed');
-        logger.error(err);
-        res.end('Error Sending Payload');
-    }
-
-    logger.info(`Response: ${responseLocation}`);
-    logger.info(`Destination: ${fileDestination}`);
-
-    rp(options)
-        .then(successful)
-        .catch(githubError);
+        const response = await axios(options);
+            logger.info(response);
+            logger.info('GIT PUT Success');
+        } catch (error) {
+            logger.error(error);
+            logger.error(error.response);
+            logger.info(error.response.data.message);
+            logger.info('GIT PUT Failed');
+        }
+        })();
 };
