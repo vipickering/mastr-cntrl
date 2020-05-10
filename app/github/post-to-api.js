61190079b294ae91f5522a51368031dee213801d
vipickering <hello@vincentp.me>
2020-05-10 20:15:33 +1200

    :construction: cleaning up code

--- app/github/post-to-api.js
+++ app/github/post-to-api.js
@@ -12,48 +12,6 @@ exports.publish = function publish(req, res, fileLocation, fileName, responseLoc
     const payloadEncoded = base64.encode(payload);
     const fileDestination = `${github.postUrl}/contents/${fileLocation}/${fileName}`;
     const messageContent = `:robot: ${commitMessage}`;
-    // const options = {
-    //     method : 'PUT',
-    //     url : fileDestination,
-    //     headers : {
-    //         Authorization : `token ${github.key}`,
-    //         'Content-Type' : 'application/vnd.github.v3+json; charset=UTF-8',
-    //         'User-Agent' : github.name
-    //     },
-    //     body : {
-    //         path : fileName,
-    //         branch : github.branch,
-    //         message : messageContent,
-    //         committer : {
-    //             'name' : github.user,
-    //             'email' : github.email
-    //         },
-    //         content : payloadEncoded
-    //     },
-    //     json : true
-    // };
-
-    // function successful() {
-    //     logger.info('Git creation successful!');
-    //     res.writeHead(201, {'location' : responseLocation});
-    //     res.end('Thanks');
-    // }
-
-    // function githubError(err) {
-    //     res.status(400);
-    //     res.send('Update failed');
-    //     logger.info('POST to Github API Failed');
-    //     logger.error(err);
-    //     res.end('Error Sending Payload');
-    // }
-
-    // logger.info(`Response: ${responseLocation}`);
-    // logger.info(`Destination: ${fileDestination}`);
-
-    // rp(options)
-    //     .then(successful)
-    //     .catch(githubError);
-
 
     (async () => {
         try {
