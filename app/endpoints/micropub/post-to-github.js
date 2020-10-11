08347fd74d411a6840e1430198984f445ee9d22e
vipickering <hello@vincentp.me>
2020-10-11 16:51:47 +1300

    :alembic: testing using UTC time

--- app/endpoints/micropub/post-to-github.js
+++ app/endpoints/micropub/post-to-github.js
@@ -37,7 +37,7 @@ exports.micropubPost = function micropubPost(req, res) {
     const responseDate = postFileNameDate.replace(/-/g, '/');
     logger.info(`add dash ${responseDate}`);
 
-    const responseLocationTime = publishedDate.slice(10, -12) + '-' + publishedDate.slice(14, -10);
+    const responseLocationTime = publishedDate.slice(10, -11) + '-' + publishedDate.slice(14, -10);
     logger.info(`responseLocationTime ${responseLocationTime}`);
 
     // Micropub Action (only fires if authentication passes)
